77b032c44c170d6cd013c91cdccc0658eab98c64
glb_hafjk <glb_hafjk@qq.com>
2019-12-22 07:42:28 +0800

    first commit hello

---
+++ src/main/java/com/shengsiyuan/springboot1222/bean/bean/Test.java
@@ -0,0 +1,7 @@
+package com.shengsiyuan.springboot1222.bean.bean;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello000000000000000000");
+    }
+}
